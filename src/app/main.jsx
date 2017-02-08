06d1cd417f7609c0507ced8a870e562584bf3ecb
Aidan <yayes.aidan@gmail.com>
2017-02-07 22:32:59 -0500

    React working

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,4 +1,13 @@
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+require('./main.html')
+
+import React from 'react'
+import {render} from 'react-dom'
+
+class App extends React.Component {
+  render () {
+    return <p> Hello React!</p>
+  }
+}
+
+render(<App/>, document.getElementById('app'))
+console.log('hi')
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,27 @@
-/**
- * Created by Aidan on 2/7/2017.
- */
+const path = require('path')
+
+module.exports = {
+  entry: './src/app/main.jsx',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './build',
+    port: 8100
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'file-loader?name=[name].[ext]!extract-loader!html-loader'
+      }
+    ]
+  }
+}
